075e04f3e1de20461f731649bd484bb9aa3bbaf2
OMpawar140 <om.pawar@mitaoe.ac.in>
2024-04-30 22:57:43 +0530

    Revert "little bit quiz fetching"
    This reverts commit 2cb00bda37fca84280f58a0871ae4183e7fe5cce.

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+//import LoginView from '@/views/LoginView.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView // Add the HomeView component here
+      component: HomeView
     },
     {
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
-    },
-    {
+    }, {
       path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue')
@@ -33,11 +34,6 @@ const router = createRouter({
       path: '/video/:videoId',
       name: 'video',
       component: () => import('../views/VideoView.vue')
-    },
-    {
-      path: '/quiz',
-      name: 'quiz',
-      component: () => import('../views/quiz.vue')
     }
   ]
 })
